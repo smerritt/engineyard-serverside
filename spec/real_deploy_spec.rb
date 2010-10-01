b703b9d4f822be43d5223ae37aecc8536f804618
Sam Merritt <smerritt@engineyard.com>
2010-10-01 15:32:20 -0700

    Update tests to reflect use of app control scripts

--- spec/real_deploy_spec.rb
+++ spec/real_deploy_spec.rb
@@ -24,58 +24,15 @@ describe "Deploying" do
   end
 
   describe "on appcloud" do
-    describe "using nginx_passenger" do
-      before(:all) {
-        @deploy_config = { "stack" => "nginx_passenger" }
-        @deployer = run_test_deploy
-      }
+    before(:all) {
+      @deploy_config = { "stack" => "nginx_passenger" }
+      @deployer = run_test_deploy
+    }
 
-      it_should_behave_like "all deploys"
-
-      it "restarts the app servers" do
-        File.exist?(File.join(@deploy_dir, 'current', 'tmp', 'restart.txt')).should be_true
-      end
+    it "restarts the app servers" do
+      File.exist?(File.join(@deploy_dir, 'current', 'restart')).should be_true
     end
 
-    describe "using nginx_mongrel" do
-      before(:all) {
-        @deploy_config = { "stack" => "nginx_mongrel", "app" => "mine" }
-        @deployer = run_test_deploy
-      }
-
-      it_should_behave_like "all deploys"
-
-      it "restarts the app servers using monit" do
-        monit_cmd = @deployer.commands.grep(/^monit/).first
-        monit_cmd.should == "monit restart all -g #{@deploy_config['app']}"
-        @deployer.command_roles[monit_cmd].should == [ :app_master, :app, :solo ]
-      end
-
-      it "uses a maintenance page when migrating" do
-        maint_page_cmds = @deployer.commands.grep(/maintenance/)
-
-        # TODO: Kind of pedestrian tests, could be better
-        maint_page_cmds.grep(/^cp/).size.should == 1
-        maint_page_cmds.grep(/^rm/).size.should == 1
-
-        maint_page_cmds.each { |c|
-          @deployer.command_roles[c].should == [ :app_master, :app, :solo ]
-        }
-      end
-    end
-
-    describe "using nginx_unicorn" do
-      before(:all) {
-        @deploy_config = { "stack" => "nginx_unicorn", "app" => "rainbowfarts" }
-        @deployer = run_test_deploy
-      }
-
-      it_should_behave_like "all deploys"
-
-      it "restarts the app servers" do
-        restart_cmd = @deployer.commands.grep(%r!/engineyard/bin/app_!).first
-        restart_cmd.should == "/engineyard/bin/app_#{@deploy_config['app']} deploy"
-      end
-    end
+    it_should_behave_like "all deploys"
   end
 end
